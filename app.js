264dd40704a11ff913572f3bc6d532ffc35d1610
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2022-10-16 00:13:40 +0900

    Dev (#2)
    * [의정] express 코드 추가
    
    * [의정] gitignore 설정
    
    * [의정] node_modules 폴더 삭제
    
    [의정] node_modules 폴더 삭제
    
    * [의정] 회원가입, 로그인 라우팅

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const router = require("./routes/routes");
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server stared. port 3000.");
 });
+
+app.use("/", router);

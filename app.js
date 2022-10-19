69182a2044d2a2c9f9ffd5d8d6514513a3b83602
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2022-10-19 13:30:50 +0900

    [의정] 회원가입 기능 구현 (#3)
    [의정] 회원가입 기능 구현

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const router = require("./routes/routes");
 
 const app = express();
 
+// 클라이언트 요청 body를 json으로 파싱 처리
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+
 app.listen(3000, () => {
   console.log("Server stared. port 3000.");
 });

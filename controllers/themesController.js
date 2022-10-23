5dab4d0d99f6497a415dc6604cd27f74904fba21
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2022-10-23 18:05:52 +0900

    [의정] 테마 목록 기능 수정

--- controllers/themesController.js
+++ controllers/themesController.js
@@ -3,7 +3,7 @@ const mysql = require("../mysql");
 // 메인 페이지
 const main = async (req, res) => {
   try {
-    //테마 정보(테마 title, image) select
+    // 테마 정보 select
     let theme = await mysql.query("themeSelectForMain");
     let theme_list = [...theme];
     res.status(200).json(theme_list);
@@ -19,17 +19,17 @@ const main = async (req, res) => {
 const theme_list = async (req, res) => {
   try {
     let theme_list = [];
-    //해당 테마 정보 select
+    // 해당 테마 정보 select
     let theme_info = await mysql.query(
       "themeSelectForThemeList",
       req.params.t_title
     );
-    //해당 테마 음식점 code, intro select
+    // 해당 테마 음식점 code, intro select
     let theme_restaurants = await mysql.query(
       "themeSelectByT_title",
       req.params.t_title
     );
-    //r_code 리스트로 해당 음식점 정보 리스트 select
+    // r_code 리스트로 해당 음식점 정보 리스트 select
     for (i of theme_restaurants) {
       let restaurant = await mysql.query("restaurantSelectByR_Idx", i.r_Idx);
       // 검색 결과 음식점 정보 하나씩 배열에 삽입
@@ -45,7 +45,6 @@ const theme_list = async (req, res) => {
           parking: !!restaurant[0].parking,
         },
       });
-      //음식점 정보 하나씩 배열에 삽입
     }
     res.status(200).json({
       t_title: theme_info[0].t_title,

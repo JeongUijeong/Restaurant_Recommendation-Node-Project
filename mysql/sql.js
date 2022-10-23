c1f2b4ec1804baff01919b464e6d52d4b77442c0
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2022-10-23 15:07:33 +0900

    [의정] 음식점 데이터 저장 (#7)

--- mysql/sql.js
+++ mysql/sql.js
@@ -1,5 +1,11 @@
 module.exports = {
+  userInsert: `insert into users (id, nickname, pw, salt) values(?,?,?,?)`,
   userSelectById: `select * from users where id=?`,
+  userSelectByToken: `select * from users where token=?`,
   userUpdateById: `update users set ? where id=?`,
   userUpdateByToken: `update users set ? where token=?`,
+  restaurantInsert: `insert into restaurants (r_Idx,r_name,image,address,tag,price,stars,parking,takeout) values (?,?,?,?,?,?,?,?,?)`,
+  likeInsert: `insert into likes (u_Idx, r_Idx) values(?,?)`,
+  likeDelete: `delete from likes where u_Idx = ? and r_Idx = ?`,
+  restaurantSelect: `select * from restaurants`,
 };

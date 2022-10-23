48a3e1651e14cc59d9b6fac1bc037de6fb9c53d4
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2022-10-23 15:06:12 +0900

    [의정] 음식점 데이터 저장

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = require("./routes/routes");
 require("dotenv").config({ path: ".env" });
 const app = express();
+const fs = require("fs");
+const jsonFile = fs.readFileSync("./data.json", "utf8");
+const jsonData = JSON.parse(jsonFile);
+const mysql = require("./mysql");
 
 // 클라이언트 요청 body를 json으로 파싱 처리
 app.use(
@@ -13,5 +17,36 @@ app.use(
 app.listen(3000, () => {
   console.log("Server stared. port 3000.");
 });
+
+const baseData = async () => {
+  try {
+    let data = await mysql.query("restaurantSelect");
+    if (!data[0]) {
+      // 음식점 데이터가 없을 때만 실행
+      for (let i = 0; i < jsonData.data.length; i++) {
+        if (jsonData.data[i].star === "NULL") {
+          jsonData.data[i].star = 0;
+        }
+        mysql.query("restaurantInsert", [
+          i,
+          jsonData.data[i].restaurant_name,
+          jsonData.data[i].img,
+          jsonData.data[i].address,
+          jsonData.data[i].tag,
+          jsonData.data[i].price,
+          jsonData.data[i].star,
+          jsonData.data[i].options.parking,
+          jsonData.data[i].options.takeout,
+        ]);
+      }
+    } else {
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+baseData();
 
 app.use("/", router);
